3e154d51b77919a97fa779f632b3656fa0919e46
Tim-1988 <tim-nat@list.ru>
2020-12-27 00:23:34 +0300

    deploy final project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Home from './Home';
+import Nav from './Nav';
+import About from './About';
+import Cart from './Cart';
+import Products from './Products';
+import Bottom from './bottom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store.js';
+
 
 function App() {
   return (
+    <Provider store={store}>
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/products" component={Products} />
+      <Route path="/about" component={About} />
+      <Route path="/cart" component={Cart} />
+      </Switch>
+      <Bottom />
     </div>
+    </Router> 
+    </Provider> 
   );
 }
+
+
 
 export default App;
